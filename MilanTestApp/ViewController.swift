003b997d321ce678b819b00b1886e198d96e8106
Milan Nosáľ <milan.nosal@hotovo.org>
2017-09-27 19:08:59 +0200

    Init commit

---
+++ MilanTestApp/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MilanTestApp
+//
+//  Created by Milan Nosáľ on 27/09/2017.
+//  Copyright © 2017 Svagant. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
